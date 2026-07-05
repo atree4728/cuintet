9ca30cf9b79bb40d08bda415179dcfc00369ac24
atree4728 <atree.public@gmail.com>
2026-07-06 02:04:04 +0900

    feat: add test for instruction fetch

--- tests/Tests/Cuintet/Core.hs
+++ tests/Tests/Cuintet/Core.hs
@@ -1,6 +1,23 @@
 module Tests.Cuintet.Core where
 
-import Prelude
+import Clash.Prelude
+import qualified Prelude as P
 
-main :: IO ()
-main = pure ()
+import Cuintet (system)
+import Cuintet.Eei
+import Test.Tasty
+import Test.Tasty.HUnit
+
+-- | Contents of sample.hex.
+sampleHex :: Vec 4 (BitVector ILen)
+sampleHex = 0x01234567 :> 0x89abcdef :> 0xdeadbeef :> 0xcafebebe :> Nil
+
+tests :: TestTree
+tests =
+  testGroup
+    "Cuintet.Core"
+    [ testCase "fetches sample.hex in order" $ do
+        let fetched = sampleN @System 6 $ system (blockRam sampleHex)
+        let memAccess = P.zipWith (P.curry Just) [0, 4, 8, 12] (toList sampleHex)
+        fetched @?= Nothing : Nothing : memAccess -- reset, delay, 0, 4, 8, 12
+    ]
--- tests/unittests.hs
+++ tests/unittests.hs
@@ -3,10 +3,11 @@ module Main where
 import Prelude
 
 import Test.Tasty
+import qualified Tests.Cuintet.Core
 
 main :: IO ()
 main =
   defaultMain $
     testGroup
       "."
-      []
+      [Tests.Cuintet.Core.tests]
