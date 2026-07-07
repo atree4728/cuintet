478009aad53ebb22b298a4460743003bd395e942
atree4728 <atree.public@gmail.com>
2026-07-07 15:28:28 +0900

    feat(corectrl): add datatype & constant

---
+++ src/Cuintet/Corectrl.hs
@@ -0,0 +1,44 @@
+module Cuintet.Corectrl where
+
+import Clash.Prelude
+
+-- | RISC-V instruction type
+data InstType
+  = XType -- undefined instruction
+  | RType
+  | IType
+  | SType
+  | BType
+  | UType
+  | JType
+  deriving (Generic, NFDataX, Show)
+
+instCode :: InstType -> BitVector 6
+instCode XType = 0b000000
+instCode RType = 0b000001
+instCode IType = 0b000010
+instCode SType = 0b000100
+instCode BType = 0b001000
+instCode UType = 0b010000
+instCode JType = 0b100000
+
+-- | Control flags of instruction.
+data InstCtrl = InstCtrl
+  { itype :: InstType
+  -- ^ Instruction type.
+  , rwbEn :: Bool
+  -- ^ Whether to enable to write back.
+  , isLui :: Bool
+  -- ^ Whether to be LUI instruction.
+  , isAluOp :: Bool
+  -- ^ Whether to use the ALU.
+  , isJump :: Bool
+  -- ^ Whether to be jump instruction.
+  , isLoad :: Bool
+  -- ^ Whether to be load instruction.
+  , funct3 :: BitVector 3
+  -- ^ @funct3@ field.
+  , funct7 :: BitVector 7
+  -- ^ @funct7@ field.
+  }
+  deriving (Generic, NFDataX, Show)
--- src/Cuintet/Eei.hs
+++ src/Cuintet/Eei.hs
@@ -33,6 +33,44 @@ data MemBusResp dataWidth = MemBusResp
 
 type MemDataWidth = 32
 type MemAddrWidth = 20
+type MemAddr = Unsigned MemAddrWidth
 
-addrToMemAddr :: Addr -> Unsigned MemAddrWidth
+addrToMemAddr :: Addr -> MemAddr
 addrToMemAddr a = truncateB (a `shiftR` natToNum @(CLog 2 (MemDataWidth `Div` 8)))
+
+data OpCode
+  = Lui
+  | AuiPc
+  | Op
+  | OpImm
+  | Jal
+  | Jalr
+  | Branch
+  | Load
+  | Store
+  deriving (Generic, NFDataX)
+
+{- ORMOLU_DISABLE -}
+opEncode :: OpCode -> BitVector 7
+opEncode Lui    = 0b0110111
+opEncode AuiPc  = 0b0010111
+opEncode Op     = 0b0110011
+opEncode OpImm  = 0b0010011
+opEncode Jal    = 0b1101111
+opEncode Jalr   = 0b1100111
+opEncode Branch = 0b1100011
+opEncode Load   = 0b0000011
+opEncode Store  = 0b0100011
+{- ORMOLU_ENABLE -}
+
+opDecode :: BitVector 7 -> OpCode
+opDecode 0b0110111 = Lui
+opDecode 0b0010111 = AuiPc
+opDecode 0b0110011 = Op
+opDecode 0b0010011 = OpImm
+opDecode 0b1101111 = Jal
+opDecode 0b1100111 = Jalr
+opDecode 0b1100011 = Branch
+opDecode 0b0000011 = Load
+opDecode 0b0100011 = Store
+opDecode _ = deepErrorX "encode: unknown opcode"
