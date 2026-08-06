e2b9bcf04d3248289b5354d7788a43569289697a
atree4728 <atree.public@gmail.com>
2026-08-06 23:00:14 +0900

    feat: S(LL|R(L|A))I?W

--- src/Cuintet/Alu.hs
+++ src/Cuintet/Alu.hs
@@ -4,30 +4,33 @@ import Clash.Prelude
 import Cuintet.CoreCtrl (InstCtrl (..), InstType (..))
 import Cuintet.Eei (IOp (..), ShiftRight (..), XLen)
 
+{- | The ALU.  A @*W@ instruction is its plain counterpart narrowed to 32 bits
+and sign-extended back, so 'exec' is width-polymorphic and @isOp32@ is decided
+in exactly one place.
+-}
 alu :: InstCtrl -> BitVector XLen -> BitVector XLen -> BitVector XLen
 alu InstCtrl {itype, isAluOp, isOp32, funct3, funct7} op1 op2
   | not isAluOp = op1 + op2
-  | otherwise = case unpack funct3 of
-      ADD | isOp32 -> signExtend $ if isSub then op1lw - op2lw else op1lw + op2lw
-      ADD -> if isSub then op1 - op2 else op1 + op2
-      SLL -> sll
-      SLT -> slt
-      SLTU -> sltu
-      XOR -> op1 `xor` op2
-      SR -> case unpack (slice d5 d5 funct7) of
-        Logical -> srl
-        Arithmetic -> sra
-      OR -> op1 .|. op2
-      AND -> op1 .&. op2
+  | isOp32 = signExtend $ exec shamt32 (truncateB op1 :: BitVector 32) (truncateB op2)
+  | otherwise = exec shamt64 op1 op2
   where
+    shamt64 = unpack $ zeroExtend (truncateB op2 :: BitVector 6)
+    shamt32 = unpack $ zeroExtend (truncateB op2 :: BitVector 5)
+
     isSub = itype /= IType && funct7 /= zeroBits
-    op1lw = truncateB op1 :: BitVector 32
-    op2lw = truncateB op2 :: BitVector 32
 
-    shiftWidth = unpack $ zeroExtend (truncateB op2 :: BitVector 6)
-    sll = op1 `shiftL` shiftWidth
-    srl = op1 `shiftR` shiftWidth
-    sra = bitCoerce $ (bitCoerce op1 :: Signed XLen) `shiftR` shiftWidth
-
-    slt = boolToBV ((bitCoerce op1 :: Signed XLen) < (bitCoerce op2 :: Signed XLen))
-    sltu = boolToBV (op1 < op2)
+    exec :: forall n' n. (KnownNat n', n ~ n' + 1) => Int -> BitVector n -> BitVector n -> BitVector n
+    exec shamt a b = case unpack funct3 of
+      ADD | isSub -> a - b
+      ADD -> a + b
+      SLL -> a `shiftL` shamt
+      SLT -> boolToBV $ signed a < signed b
+      SLTU -> boolToBV $ a < b
+      XOR -> a `xor` b
+      SR -> case unpack (slice d5 d5 funct7) of
+        Logical -> a `shiftR` shamt
+        Arithmetic -> pack $ signed a `shiftR` shamt
+      OR -> a .|. b
+      AND -> a .&. b
+      where
+        signed x = bitCoerce x :: Signed n
